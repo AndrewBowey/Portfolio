1f6bc3efc56c9d397928ebe8813e9786bdb198b9
AndrewBowey <a888bomb@gmail.com>
2022-11-07 00:10:14 +0000

    Added navbar with placeholder logo and links.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from "./Pages/NotFound";
 import Dashboard from "./Pages/Dashboard.js";
+import NavBar from "./Components/Navbar";
 
 function App() {
   return (
     <Router>
-      {/* <NavBar /> */}
+      <NavBar />
       <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+
         <Route path="/contact" />
-        <Route path="/Certificates and recognition" />
+        <Route path="/certsandrecs" />
         <Route path="/resume" />
         <Route path="/projects" />
-        <Route exact path="/" element={<Dashboard />} />
-        <Route element={<NotFound />} />
       </Routes>
     </Router>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,55 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import logo from "../logo.jpg";
+import Button from "react-bootstrap/esm/Button";
 function NavBar() {
-  return;
+  const navigate = useNavigate();
+  return (
+    <Navbar
+      sticky="top"
+      variant="dark"
+      bg="dark"
+      style={{ fontFamily: "Inter" }}
+    >
+      <Container fluid>
+        {/* <Button
+          variant="light"
+          onClick={() => {
+            navigate.goBack();
+          }}
+        >
+          Back
+        </Button> */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Full Stack Developer"
+          />
+        </Navbar.Brand>
+        <Navbar.Text className="me-auto">Andrew Bowey</Navbar.Text>
+        <Nav className="justify-content-end">
+          <Nav.Link as={Link} to="/projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link as={Link} to="/resume">
+            Resume
+          </Nav.Link>
+          <Nav.Link as={Link} to="/certsandrecs/">
+            Certificates and Recognition
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
